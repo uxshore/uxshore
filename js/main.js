af2bc180d5e6ef7631f2ad97c23a1b8004ae1586
Bryonpectol <46723917+Bryonpectol@users.noreply.github.com>
2024-03-23 23:57:28 -0600

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ let logoRanNum;
 logoRanNum = Math.floor(Math.random() * (5 - 1) + 1);
 
 let desktopLogo = document.getElementById("desktoplogo");
+let mobileLogo = document.getElementById("mobilelogo");
 
 
 desktopLogo.setAttribute("src", "/images/icons/" + logoRanNum + ".png");
+mobileLogo.setAttribute("src", "/images/icons/" + logoRanNum + ".png");
