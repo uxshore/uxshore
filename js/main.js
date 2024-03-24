b12d1e41b93db29181ccfa47b325379b9a752b0a
bryon <bryon.pectol@gmail.com>
2024-03-23 23:19:03 -0600

    test

--- js/main.js
+++ js/main.js
@@ -4,4 +4,5 @@ logoRanNum = Math.floor(Math.random() * (5 - 1) + 1);
 
 let desktopLogo = document.getElementById("desktoplogo");
 
+
 desktopLogo.setAttribute("src", "/images/icons/" + logoRanNum + ".png");
