da38e325c58b43a7b3684a10a9b26564c43e3f6d
bryon <bryon.pectol@gmail.com>
2024-03-23 23:16:57 -0600

    test commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let logoRanNum;
+
+logoRanNum = Math.floor(Math.random() * (5 - 1) + 1);
+
+let desktopLogo = document.getElementById("desktoplogo");
+
+desktopLogo.setAttribute("src", "/images/icons/" + logoRanNum + ".png");
